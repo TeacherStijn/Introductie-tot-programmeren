664c07871f0ef22eb2dbd78ebba0d0684a43d52c
Stijn Janssen <stinow@gmail.com>
2022-01-08 15:10:46 +0100

    Create autodemo.java

---
+++ autodemo.java
@@ -0,0 +1,16 @@
+class Auto {
+	private String merk;
+	private String type;
+	private String kleur;
+	
+	public void starten() {
+		System.out.println("Deze " + this.merk + " gaat rijden!");
+	}
+}
+
+// Om deze klasse te gebruiken, dienen wij nog:
+// object van de klasse te maken, ook wel 'instantiëren' van de klasse middels:
+
+// new Auto.
+
+// Ook dient er een main methode te zijn die de boel opstart 
